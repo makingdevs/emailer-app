81e78d9d711e89ccf356e2432c5be6144e927627
carlogilmar <carlogilmar12@gmail.com>
2016-10-04 13:14:25 -0500

    Añadiendo respuesta

--- webserver.groovy
+++ webserver.groovy
@@ -1,11 +1,21 @@
 import io.vertx.groovy.ext.web.handler.StaticHandler
 import io.vertx.groovy.ext.web.Router
+import io.vertx.groovy.ext.web.handler.BodyHandler
+import io.vertx.core.json.Json
 
 def server = vertx.createHttpServer()
 def router = Router.router(vertx)
+router.route().handler(BodyHandler.create())
 
 router.route("/static/*").handler(StaticHandler.create())
 
+router.post("/some").handler { routingContext ->
+  println routingContext.properties
+  routingContext.response()
+    .setStatusCode(201)
+    .putHeader("content-type", "application/json; charset=utf-8")
+    .end(Json.encodePrettily(routingContext.bodyAsJson))
+}
 
 
 server.requestHandler(router.&accept).listen(8080)
