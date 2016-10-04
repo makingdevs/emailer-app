d5f405cc7ea39d1bfc47b6da5aefa6505848701e
carlogilmar <carlogilmar12@gmail.com>
2016-10-04 12:26:16 -0500

    Add Router, and requestHandler

--- webserver.groovy
+++ webserver.groovy
@@ -1,8 +1,11 @@
 import io.vertx.groovy.ext.web.handler.StaticHandler
+import io.vertx.groovy.ext.web.Router
 
 def server = vertx.createHttpServer()
+def router = Router.router(vertx)
 
 router.route("/static/*").handler(StaticHandler.create())
 
 
-server.listen(8080)
+
+server.requestHandler(router.&accept).listen(8080)
