77c375fc2e50f54cfbcaf7d9e1b9c70a898e715e
carlogilmar <carlogilmar12@gmail.com>
2016-10-06 12:02:40 -0500

    delete server test

--- server.groovy
+++
@@ -1,9 +0,0 @@
-import io.vertx.groovy.ext.asyncsql.MySQLClient
-
-// To create a MySQL client:
-
-def mySQLClientConfig = [
-  host:"http://localhost:3306/vertx_test"
-	]
-def mySQLClient = MySQLClient.createShared(vertx, mySQLClientConfig)
-
