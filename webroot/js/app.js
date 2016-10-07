f590496d5ace38bfe8e1f36171c6b33fd60bb066
carlogilmar <carlogilmar12@gmail.com>
2016-10-07 11:42:10 -0500

    Add simple test for use Handlebars

--- webroot/js/app.js
+++ webroot/js/app.js
@@ -1,3 +1,8 @@
 $(function() {
-  console.log("Hola inmundo");
+  console.log("Realizando la primera funcion");
+  var source = $("#entry-template").html()
+  var template = Handlebars.compile(source)
+  var mail = { receiver:"gilmar@makingdevs.com", sender:"carlo@makingdevs.com", title:"Saludos", subject:"Joli", content:"Saludos a to2" }
+  var html = template(mail);
+  $("div.row:first").prepend(html);
 });
