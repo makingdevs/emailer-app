ed4f6c77c5c7ac4bdb3ffe2ff1d3a8a7cd673d12
carlogilmar <carlogilmar12@gmail.com>
2016-10-07 12:19:47 -0500

    Add simple test for impress an Json Array with handlebars

--- webroot/js/app.js
+++ webroot/js/app.js
@@ -2,7 +2,13 @@ $(function() {
   console.log("Realizando la primera funcion");
   var source = $("#entry-template").html()
   var template = Handlebars.compile(source)
-  var mail = { receiver:"gilmar@makingdevs.com", sender:"carlo@makingdevs.com", title:"Saludos", subject:"Joli", content:"Saludos a to2" }
-  var html = template(mail);
+  var mail = [
+             { receiver:"gilmar@makingdevs.com", sender:"carlo@makingdevs.com", title:"Saludos", subject:"Joli", content:"Saludos a to2" },
+             { receiver:"1gilmar@makingdevs.com", sender:"1carlo@makingdevs.com", title:"1Saludos", subject:"1Joli", content:"1Saludos a to2" }];
+
+  var wrapper={emails:mail}
+
+  var html = template(wrapper);
+
   $("div.row:first").prepend(html);
 });
