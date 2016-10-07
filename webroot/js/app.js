4da7807d4a9aa7575d759c0e9a9505351c9e0c12
carlogilmar <carlogilmar12@gmail.com>
2016-10-07 14:56:40 -0500

    Try to pull the dataJson from mongo

--- webroot/js/app.js
+++ webroot/js/app.js
@@ -1,14 +1,15 @@
 $(function() {
   console.log("Realizando la primera funcion");
+
   var source = $("#entry-template").html()
   var template = Handlebars.compile(source)
-  var mail = [
-             { receiver:"gilmar@makingdevs.com", sender:"carlo@makingdevs.com", title:"Saludos", subject:"Joli", content:"Saludos a to2" },
-             { receiver:"1gilmar@makingdevs.com", sender:"1carlo@makingdevs.com", title:"1Saludos", subject:"1Joli", content:"1Saludos a to2" }];
+  var mail = $.getJSON("http://localhost:8080/show")
 
-  var wrapper={emails:mail}
+  console.log("Get Json ok");
+  console.log(mail);
+  console.log(mail.responseJSON);
 
-  var html = template(wrapper);
-
-  $("div.row:first").prepend(html);
+  //var wrapper={emails:mail.responseJSON}
+  //var html = template(wrapper);
+  //$("div.row:first").prepend(html);
 });
