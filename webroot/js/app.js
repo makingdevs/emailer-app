b71179d275a77fbce4be2d3f4f1d5e3c288927bc
carlogilmar <carlogilmar12@gmail.com>
2016-10-10 12:25:09 -0500

    implement Ajax-post

--- webroot/js/app.js
+++ webroot/js/app.js
@@ -1,3 +1,4 @@
+//------------------------------------------------------------------------funcion que recupera todos los registros y pinta la tabla
 var findAll = function(){
   var a=  $.ajax({
       url:"http://localhost:8080/show",
@@ -13,19 +14,40 @@ var findAll = function(){
       }
     });
 }
+
+//-------------------------------------------------------------------------------------------New Email
 var newEmail = function(){ console.log("new email"); }
-var show = function(id){
+
+//-------------------------------------------------------------------------------------------Show Email
+var showOne = function(id){
   console.log("show email"+ id);
   $('input[name="email_1"]').val(id);
+
+//----------------------------------------------------ajax
+  var a = $.ajax({
+    data: "idEmail="+id,
+    url: 'http://localhost:8080/one',
+    type: 'post',
+    beforeSend: function () {
+      console.log("Procesando, aguanta");
+    },
+    success: function (response) {
+      console.log("Ya estuvo:D"+response);
+    }
+  });
+
 }
+
 var deleteEmail = function(id){ console.log("delete email " + id); }
 
+//------------------------------------------------------------------------routes DirectorJS
 var routes = {
   '/': findAll,
   '/new': newEmail,
-  '/show/:mailId': show,
+  '/showOne/:mailId': showOne,
   '/delete/:mailId': deleteEmail
 };
+
 var router = Router(routes);
 router.init();
 
