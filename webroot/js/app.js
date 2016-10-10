ed0dbd30bb04df4b98e2a5ae58c3ce9589f3d246
carlogilmar <carlogilmar12@gmail.com>
2016-10-10 10:21:50 -0500

    Add director JS

--- webroot/js/app.js
+++ webroot/js/app.js
@@ -1,19 +1,31 @@
-$(function() {
-  console.log("Realizando la primera funcion");
+var findAll = function(){
   var a=  $.ajax({
       url:"http://localhost:8080/show",
-      type:'POST',
+      type:'GET',
       dataType: 'json',
       success:
         function(response){
-  //        console.log(response)
-          var source = $("#entry-template").html()
-          var template = Handlebars.compile(source)
-          var wrapper={emails:response}
+          var source = $("#entry-template").html();
+          var template = Handlebars.compile(source);
+          var wrapper={emails:response};
           var html = template(wrapper);
           $("div.row:first").prepend(html);
-  //        console.log(html)
       }
-    })
+    });
+}
+var newEmail = function(){ console.log("new email"); }
+var show = function(id){
+  console.log("show email"+ id);
+  $('input[name="email_1"]').val(id);
+}
+var deleteEmail = function(id){ console.log("delete email " + id); }
 
-});
+var routes = {
+  '/': findAll,
+  '/new': newEmail,
+  '/show/:mailId': show,
+  '/delete/:mailId': deleteEmail
+};
+var router = Router(routes);
+router.init();
+
