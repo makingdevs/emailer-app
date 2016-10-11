b9549e398764c7015bd6979121dd2d6fd331bceb
carlogilmar <carlogilmar12@gmail.com>
2016-10-11 14:07:06 -0500

    Add jquery for play with elements

--- webroot/js/app.js
+++ webroot/js/app.js
@@ -6,6 +6,7 @@ var findAll = function(){
       dataType: 'json',
       success:
         function(response){
+          $(".jumbotron").hide()
           var source = $("#entry-template").html();
           var template = Handlebars.compile(source);
           var wrapper={emails:response};
@@ -17,10 +18,14 @@ var findAll = function(){
 
 //-------------------------------------------------------New Email
 var newEmail = function(){
-  console.log("new email");
   //limpiando los input
+  $("#reader").hide()
+  $(".jumbotron").show()
   $('input').each(function(){ $(this).val(''); })
-  $("form").attr("action","/newEmail")
+  $("textarea").val("");
+  $("form").attr("action","/newEmail");
+  $(".jumbotron").css("background","#b0bec5");
+
 }
 
 //-------------------------------------------------------Show Email
@@ -33,6 +38,7 @@ var showOne = function(id){
           //parseando JSON
           var json=$.parseJSON(response);
           //inputs 1by1
+          $(".jumbotron").show()
           $('input[name="email_id"]').val(json._id);
           $('input[name="email_1"]').val(json.receiver);
           $('input[name="email_2"]').val(json.sender);
@@ -40,6 +46,7 @@ var showOne = function(id){
           $('input[name="title"]').val(json.title);
           $('textarea').val(json.content);
           //change the form action
+          $(".jumbotron").css("background","#eceff1");
           $("form").attr("action","/update")
       }
   });
@@ -53,6 +60,9 @@ var deleteEmail = function(id){
     url: 'http://localhost:8080/remove',
     type: 'post',
     success: function (response) {
+    alert("Se elimino registro");
+    $("#reader").hide()
+    window.location.href = "http://localhost:8080/static/#/";
     }
   });
 
