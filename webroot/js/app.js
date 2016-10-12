e56aaaef804dbb8b82054b0604432121a29f5fd6
carlogilmar <carlogilmar12@gmail.com>
2016-10-12 12:22:49 -0500

    Use method submit for update and add new email

--- webroot/js/app.js
+++ webroot/js/app.js
@@ -1,92 +1,88 @@
 $( document ).ready(function() {
-   $(".jumbotron").hide()
+  $(".jumbotron").hide();
+  $("#reader").hide();
 });
 
-var findAll = function(){
-  $.ajax({
-      url:"http://localhost:8080/show",
-      type:'GET',
-      dataType: 'json',
-      success:
-        function(response){
-          $(".jumbotron").hide();
-          $("#start").hide();
-          var source = $("#entry-template").html();
-          var template = Handlebars.compile(source);
-          var wrapper={emails:response};
-          var html = template(wrapper);
-          $("div.row:first").prepend(html);
-      }
-    });
+//---------------------------------------------MyFunctions
+function readerForms(){
+  $("#reader").hide();
+  $("#start").hide();
+  $(".jumbotron").show();
+  $('input').each(function(){ $(this).val(''); });
+  $("textarea").val("");
 }
 
-//-------------------------------------------------------New Email
+function readerEmails(){
+  $.ajax({
+    url:"http://localhost:8080/show",
+    type:'GET',
+    dataType: 'json',
+    success:
+      function(response){
+        $(".jumbotron").hide();
+        $("#start").hide();
+        var source = $("#entry-template").html();
+        var template = Handlebars.compile(source);
+        var wrapper={emails:response};
+        var html = template(wrapper);
+        $("div.row:first").prepend(html);
+      }
+  });
+}
+
+//-------------------------------------------Routes
+var findAll = function(){
+   readerEmails();
+}
+
 var newEmail = function(){
-  //limpiando los input
-  $("#reader").hide()
-  $("#start").hide();
-  $(".jumbotron").show()
-  $('input').each(function(){ $(this).val(''); })
-  $("textarea").val("");
-//  $("form").attr("action","/newEmail");
+  readerForms();
   $(".jumbotron").css("background","#b0bec5");
-//--------------------------------------------mandando datos del formulario
-  $("#sendData").click(function(){
-       $.ajax({
-      data: $("#data").serialize(),
+  $( "#mail_form" ).submit(function( event ) {
+    event.preventDefault();
+     $.ajax({
+      data: $("#mail_form").serialize(),
       type: 'post',
       url: 'http://localhost:8080/newEmail',
       success: function(){
-        alert("Email agregado");
-        window.location.href = "http://localhost:8080/static/#/";
+        alert("Email agregado exitosamente");
       }
     });
-
-
   });
+}
 
-}//end
-
-//-------------------------------------------------------Show Email
 var showOne = function(id){
+  $("#reader").hide();
   $.ajax({
     data: "idEmail="+id,
     url: 'http://localhost:8080/one',
     type: 'post',
     success: function (response) {
-          //parseando JSON
-          var json=$.parseJSON(response);
-          //inputs 1by1
-          $(".jumbotron").show()
-          $('input[name="email_id"]').val(json._id);
-          $('input[name="email_1"]').val(json.receiver);
-          $('input[name="email_2"]').val(json.sender);
-          $('input[name="asunto"]').val(json.submit);
-          $('input[name="title"]').val(json.title);
-          $('textarea').val(json.content);
-          //change the form action
-          $(".jumbotron").css("background","#eceff1");
-          //$("form").attr("action","/update")
-
-          //---------------------------ajax request
-            $("#sendData").on('click',function(){
-              alert("Actualizando");
-              $.ajax({
-                data: $("#data").serialize(),
-                type: 'post',
-                url: 'http://localhost:8080/update',
-                success: function(){
-                  alert("Email Actualizado");
-                  window.location.href = "http://localhost:8080/static/#/";
-                }
-              });
-      })
+      var json=$.parseJSON(response);
+      $(".jumbotron").show()
+      $('input[name="email_id"]').val(json._id);
+      $('input[name="email_1"]').val(json.receiver);
+      $('input[name="email_2"]').val(json.sender);
+      $('input[name="asunto"]').val(json.submit);
+      $('input[name="title"]').val(json.title);
+      $('textarea').val(json.content);
+      $(".jumbotron").css("background","#eceff1");
     }
   });
 
+  $( "#mail_form" ).submit(function( event ) {
+    event.preventDefault();
+    $.ajax({
+      data: $("#mail_form").serialize(),
+      type: 'post',
+      url: 'http://localhost:8080/update',
+      success: function(){
+        alert("Email Actualizado");
+      }
+    });
+  });
 }
 
-//------------------------------------------------Delete Email
 var deleteEmail = function(id){
   $.ajax({
     data: "idEmailRemove="+id,
@@ -94,15 +90,12 @@ var deleteEmail = function(id){
     type: 'post',
     success: function (response) {
     alert("Se elimino registro");
-    $("#reader").hide()
-    window.location.href = "http://localhost:8080/static/#/";
     }
   });
-
-
+  $("#reader").hide()
+  readerEmails();
 }
 
-//------------------------------------------------------------------------routes DirectorJS
 var routes = {
   '/': findAll,
   '/new': newEmail,
