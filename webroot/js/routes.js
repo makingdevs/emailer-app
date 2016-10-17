b95b5b4252b5e43e9140bdaf65bc15b3519087b7
carlogilmar <carlogilmar12@gmail.com>
2016-10-16 20:09:58 -0500

    Add Jquery for hide and show divs

--- webroot/js/routes.js
+++ webroot/js/routes.js
@@ -1,12 +1,21 @@
 //JS for all routes
 
+// static route-->read Emails
 var readEmails=function(){
 	alert("Leyendo Emails");
+	//show and hide divs
+	$("#start").hide();
+	$("#formEmails").hide();
+	$("#readEmails").show();
 	sendReadEmails();
 }
 
 var newAdd=function(){
 	alert("Mostrando Forma para Agregar un correo nuevo");
+	//show and hide divs
+	$("#start").hide();
+	$("#formEmails").show();
+	$("#readEmails").hide();
 }
 
 //Routes Director JS
