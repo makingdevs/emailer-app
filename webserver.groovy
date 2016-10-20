b394698cc377e4eb6affb0d24470b3bfcd3b5635
carlogilmar <carlogilmar12@gmail.com>
2016-10-20 13:01:26 -0500

    #17 clean my code, and refcator update route

--- webserver.groovy
+++ webserver.groovy
@@ -16,18 +16,14 @@ if(!config.mail || !config.mongo)
 def mailClient = MailClient.createShared(vertx, config.mail)
 def mongoClient = MongoClient.createShared(vertx, config.mongo)
 
-//vertx routes
 def server = vertx.createHttpServer()
 def router = Router.router(vertx)
-
 router.route().handler(BodyHandler.create())
 
-//route by defect->index
 router.route("/static/*").handler(
 	 StaticHandler.create().setCachingEnabled(false)
 )
 
-//route by insert new Email to MongoDb
 router.post("/newEmail").handler { routingContext ->
     def params = routingContext.request().params()
     def email = [
@@ -37,7 +33,6 @@ router.post("/newEmail").handler { routingContext ->
       lastUpdated:new Date().time,
       version:1
     ]
-
 		mongoClient.save("email_storage", email, { id ->
       routingContext.response()
       .setStatusCode(201)
@@ -46,7 +41,6 @@ router.post("/newEmail").handler { routingContext ->
     })
 }
 
-//route for show all items
 router.route("/show").handler({ routingContext ->
 		def query = [:]
 		mongoClient.find("email_storage", query, { res ->
@@ -60,7 +54,6 @@ router.route("/show").handler({ routingContext ->
 				})
 	})
 
-//route for remove one Email
 router.post("/remove").handler { routingContext ->
 	def emailRemove= routingContext.request().getParam("idEmail")
 		def query = ["_id":emailRemove]
@@ -76,7 +69,6 @@ router.post("/remove").handler { routingContext ->
 				})
 }
 
-//route for show one email
 router.post("/showEmail").handler { routingContext ->
 	def idEmail= routingContext.request().getParam("idEmail")
 		def query = ["_id":idEmail]
@@ -95,29 +87,18 @@ router.post("/showEmail").handler { routingContext ->
 				})
 }
 
-//route for receive a petition and update an email
 router.post("/update").handler { routingContext ->
 
-
-	//recuperando los datos del formulario
-	  def emailToUpdate= routingContext.request().getParam("email_id")
-		def submitInput= routingContext.request().getParam("subjectEmail")
-		def contentText= routingContext.request().getParam("contentEmail")
-		def versionEmail= routingContext.request().getParam("versionEmail")
-
-    def newVersion= versionEmail.toInteger() + 1
-    def lastUpdate=(new Date()).toString()
-
-		def query = ["_id":emailToUpdate]
-		def update = [
+    def paramsUpdate=routingContext.request().params()
+    def query = ["_id":paramsUpdate.email_id]
+    def update = [
 		$set:[
-		submit:submitInput,
-		content:contentText,
-    version:newVersion,
-    lastUpdate:lastUpdate
+          subject:paramsUpdate.subjectEmail,
+          content:paramsUpdate.contentEmail,
+          version:paramsUpdate.versionEmail.toInteger() +1,
+          lastUpdate:new Date().time
+	    	]
 		]
-		]
-
 		mongoClient.update("email_storage", query, update, { res ->
 				if (res.succeeded()) {
 						routingContext.response()
@@ -130,10 +111,8 @@ router.post("/update").handler { routingContext ->
 		})
 }
 
-//router by send the total of documents
 router.route("/countTotal").handler({ routingContext ->
   def query = [:]
-  //conteo de numeros
   mongoClient.count("email_storage",query,{res ->
     if(res.succeeded()){
       routingContext.response()
@@ -143,15 +122,14 @@ router.route("/countTotal").handler({ routingContext ->
   })
 })
 
-//router for show only a set of values
 router.post("/showSet").handler { routingContext ->
   def setValue=0
   setValue= routingContext.request().getParam("setValue")
   def query = [:]
   def options=[
-  limit:10,
-  skip:setValue.toInteger()
-  ]
+      limit:10,
+      skip:setValue.toInteger()
+      ]
   mongoClient.findWithOptions("email_storage", query, options, { res ->
     if (res.succeeded()) {
       routingContext.response()
@@ -163,32 +141,21 @@ router.post("/showSet").handler { routingContext ->
   })
 }
 
-//route for send an Email
 router.post("/send").handler { routingContext ->
-
-
-   	//obteniendo los campos del input
 
 	  def idTemplate= routingContext.request().getParam("email_id")
 	  def emailToSend= routingContext.request().getParam("emailPreview")
-
     def query = ["_id":idTemplate]
-
-		//buscando idTemplate en Mongo
 		mongoClient.find("email_storage", query, { res ->
 				if (res.succeeded()) {
 						res.result().each { json ->
 						def jsonEmail =groovy.json.JsonOutput.toJson(json)//regresando el json del template
-            //println "respuesta:  "+json["version"]
-            //armar el correo
             def message = [:]
             message.from = "emailer@app.com"
             message.to = emailToSend
-            message.subject = json["submit"]
+            message.subject = json["subject"]
             //message.cc = "carlogilmar12@gmail.com"
             message.html = json["content"]
-
-						//Mandando el correo
             mailClient.sendMail(message, { result ->
               if (result.succeeded()) {
                 println(result.result())
@@ -206,8 +173,6 @@ router.post("/send").handler { routingContext ->
 				}
 				})
 }
-
-
 
 server.requestHandler(router.&accept).listen(8080)
 
