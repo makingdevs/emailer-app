b8965ace4cb0a50259915a1b9bbdaea8ede1ef38
carlogilmar <carlogilmar12@gmail.com>
2016-10-27 14:51:54 -0500

    #16 The service can receive params, and implements Template Engine for add the params to emailer template

--- webserver.groovy
+++ webserver.groovy
@@ -198,15 +198,29 @@ router.post("/serviceEmail").handler { routingContext ->
       res.result().each { json ->
         //recuperando y armando el correo
         def jsonEmail =groovy.json.JsonOutput.toJson(json)//regresando el json del template
+        //Match con Engine-------
+        println "Recuperando Params"
+        println jsonResponse["params"]
+        //Obtener el contenido
+        def params=jsonResponse["params"]
+        //Inicializar el engine
+        def engine=new groovy.text.SimpleTemplateEngine()
+        //hacer el match, e imprimir el resultado
+        def contentEmail=engine.createTemplate(json["content"]).make(params)
+        println "Aqui lo nuevo producido por los parametros:"
+        println contentEmail.properties
+
         //Armando el correo a enviar con los datos de
         def message = [:]
         message.from = "emailer@app.com"
         message.to = jsonResponse["to"]//de la peticion
         message.subject = jsonResponse["subject"]//de la peticion
-        message.html = json["content"]//del emailer en db
+        //message.html = json["content"]//del emailer en db
+        message.html = contentEmail.toString()//del emailer en db
         //Evaluando si hay cc y cco
         if(jsonResponse["cc"]) message.cc = jsonResponse["cc"]//de la peticion
         if(jsonResponse["cco"]) message.bcc = jsonResponse["cco"]//de la peticion
+
 
         //enviando correo
         mailClient.sendMail(message, { result ->
