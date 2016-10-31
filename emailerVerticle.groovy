f2287e65bac3e66856f50d22690c97538037437c
carlogilmar <carlogilmar12@gmail.com>
2016-10-31 13:21:43 -0600

    #17 Add config for mongo, and a consumer for persist data

--- emailerVerticle.groovy
+++ emailerVerticle.groovy
@@ -1,9 +1,25 @@
-def eb = vertx.eventBus()
+import io.vertx.core.json.Json
+//mongo
+import io.vertx.groovy.core.Vertx
+import io.vertx.groovy.ext.mongo.MongoClient
 
+//Mongo DB
+
+def config = Vertx.currentContext().config()
+def mongoClient = MongoClient.createShared(vertx, config.mongo)
+
+
+
+//Event Bus Born
+def eb = vertx.eventBus()
 
 //Handlers for Event bus
 eb.consumer("com.makingdevs.emailer.new", { message ->
     println("I have received a message: ${message.body()}")
+    mongoClient.save("email_storage", message.body(), { id ->
+      println "añadiendo email, checar en mongo directamente"
+    })
+
 })
 
 eb.consumer("com.makingdevs.emailer.show.total", { message ->
