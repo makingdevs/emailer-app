6b2705f662e9d8107b9bf9900ee647e7bbfcbfc4
carlogilmar <carlogilmar12@gmail.com>
2016-11-03 13:20:56 -0600

    #22 Clean my code

--- webroot/js/verticle.js
+++ webroot/js/verticle.js
@@ -2,7 +2,6 @@ var eb = new EventBus('http://localhost:8080/eventbus');
 eb.onopen = function() {
   eb.registerHandler('com.makingdevs.email.success', function(error, message) {
     var response=JSON.stringify(message.body);
-    console.log("Se envio esto: "+response);
     notie.alert(1, '<small>El correo se ha enviado con los siguientes datos: '+response+'</small>', 3);
   });
 }
