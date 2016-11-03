8a13c223ee06057c6d94f5acd59e3ad226484317
carlogilmar <carlogilmar12@gmail.com>
2016-11-03 11:59:34 -0600

    #22 Modify messages for Js

--- senderVerticle.groovy
+++ senderVerticle.groovy
@@ -18,6 +18,7 @@ eb.consumer("com.makingdevs.emailer.send.email", { message ->
     //Mandar email
     mailClient.sendMail(mail, { result ->
       if (result.succeeded()) {
+        vertx.eventBus().send("com.makingdevs.email.success", "Se ha enviado el correo exitosamente")
         println("Preview enviado, checa tu correo")
       } else {
         result.cause().printStackTrace()
@@ -44,6 +45,7 @@ eb.consumer("com.makingdevs.emailer.send.service", { message ->
   mailClient.sendMail(mail, { result ->
     if (result.succeeded()) {
       println("Mail enviado, checa tu correo")
+  vertx.eventBus().send("com.makingdevs.email.success", [to:mail.from, subject:mail.to, id:"niohnio"])
     } else {
       result.cause().printStackTrace()
     }
