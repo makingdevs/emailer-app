a85a38987334266aa2903cea14b534d216fce3a4
carlogilmar <carlogilmar12@gmail.com>
2016-11-03 12:00:32 -0600

    #22 Receive a json file

--- webroot/js/verticle.js
+++ webroot/js/verticle.js
@@ -1,6 +1,8 @@
 var eb = new EventBus('http://localhost:8080/eventbus');
 eb.onopen = function() {
-  eb.registerHandler('chat.to.client', function(error, message) {
-    console.log('received a message: '+message.body);
+  eb.registerHandler('com.makingdevs.email.success', function(error, message) {
+    var response=JSON.stringify(message.body);
+    console.log("Se envio esto: "+response);
+    console.log('We send message this subject: '+response.to);
   });
 }
