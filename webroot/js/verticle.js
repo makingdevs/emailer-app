ecd14e28bdfbdb164cbb54953bf6d41077812e4a
carlogilmar <carlogilmar12@gmail.com>
2016-11-03 12:49:44 -0600

    #22 Add notie test

--- webroot/js/verticle.js
+++ webroot/js/verticle.js
@@ -3,6 +3,6 @@ eb.onopen = function() {
   eb.registerHandler('com.makingdevs.email.success', function(error, message) {
     var response=JSON.stringify(message.body);
     console.log("Se envio esto: "+response);
-    console.log('We send message this subject: '+response.to);
+    notie.alert(1, '<small>El correo se ha enviado con los siguientes datos: '+response+'</small>', 3);
   });
 }
