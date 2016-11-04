7b63c964348feb9fc1ae97bef3e09050b90e98a0
carlogilmar <carlogilmar12@gmail.com>
2016-11-04 15:06:53 -0600

    #23 Modify the message of the notify

--- webroot/js/verticle.js
+++ webroot/js/verticle.js
@@ -1,10 +1,10 @@
 var eb = new EventBus('http://localhost:8080/eventbus');
 eb.onopen = function() {
   eb.registerHandler('com.makingdevs.email.success', function(error, message) {
-    var response=JSON.stringify(message.body);
+
     new PNotify({
       title: 'Se ha enviado el correo exitosamente.',
-      text: 'Hemos enviado los siguientes datos:'+message.body,
+      text: 'Mensaje: '+message.body,
       type: 'success'
     });
   });
