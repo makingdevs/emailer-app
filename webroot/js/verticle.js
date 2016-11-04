68f098e164664f9a655636a6fecad88756961fba
carlogilmar <carlogilmar12@gmail.com>
2016-11-04 14:31:14 -0600

    #23 Apply notify

--- webroot/js/verticle.js
+++ webroot/js/verticle.js
@@ -2,6 +2,10 @@ var eb = new EventBus('http://localhost:8080/eventbus');
 eb.onopen = function() {
   eb.registerHandler('com.makingdevs.email.success', function(error, message) {
     var response=JSON.stringify(message.body);
-    notie.alert(1, '<small>El correo se ha enviado con los siguientes datos: '+response+'</small>', 3);
+    new PNotify({
+      title: 'Se ha enviado el correo exitosamente.',
+      text: 'Hemos enviado los siguientes datos:'+message.body,
+      type: 'success'
+    });
   });
 }
