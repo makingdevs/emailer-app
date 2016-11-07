71152b8e2a5efb42aa77f0e195db248a80d87a74
carlogilmar <carlogilmar12@gmail.com>
2016-11-07 11:30:48 -0600

    #24 Create http client for test

--- test.groovy
+++ test.groovy
@@ -1,25 +1,55 @@
 import io.vertx.groovy.ext.unit.TestSuite
 import io.vertx.groovy.core.Vertx
+import io.vertx.groovy.core.http.HttpClient
 
+/************************************
+Please run as webserver...
+vertx run test.groovy -conf conf.json
+************************************/
+
+//Adaptar la configuración externalizada
 def config = Vertx.currentContext().config()
-  //configuracion externalizada
   opt =[
   "config":config
   ]
 
-
+//Configuración para la TestSuite
 def options = [
-reporters:[
-[
-to:"console"
-]
-]
+    reporters:
+    [
+       [ to:"console" ]
+    ]
 ]
 
+//Create TestSuite
 def suite = TestSuite.create("the_test_suite")
 
+//Deploy webserver
 suite.before({ context ->
   vertx.deployVerticle("webserver.groovy",opt)
 })
 
+suite.test("testCaseCountable", { context ->
+    def msg="hola"
+    context.assertEquals("hola", msg)
+})
+
+
+//--------------------------------- Start TestCase
+suite.test("countable_test", { context ->
+  // Send a request and get a response
+  def client = vertx.createHttpClient()
+  def async = context.async()
+  client.getNow(8080, "localhost", "/", { resp ->
+    resp.bodyHandler({ body ->
+      context.assertEquals("No resources found", body.toString("UTF-8"))
+    })
+    client.close()
+    async.complete()
+  })
+})
+
+
+
+//Run Suite
 suite.run(options)
