cc40a2974a8d7efcc0200f0d4fe9813908bd9cf1
carlogilmar <carlogilmar12@gmail.com>
2016-11-10 11:08:37 -0600

    #24 Cambiando comunicación por nuevos verticles

--- senderVerticle.groovy
+++ senderVerticle.groovy
@@ -9,64 +9,6 @@ def mailClient = MailClient.createShared(vertx, config.mail)
 
 def eb = vertx.eventBus()
 
-//Consumer para mandar email de prueba
-eb.consumer("com.makingdevs.emailer.send.email", { message ->
-    //formando el email
-    def mail=[:]
-    mail.from="emailer@app.com"
-    mail.to=message.body().to
-    mail.subject=message.body().subject
-    mail.html=message.body().content
-
-    def response="Hemos enviado lo siguiente:\n TEMPLATE ID:"+message.body().id+".\n DESTINATARIO: "+mail.to+"\n SUBJECT: "+mail.subject
-    //Mandar email
-    mailClient.sendMail(mail, { result ->
-      if (result.succeeded()) {
-        vertx.eventBus().send("com.makingdevs.email.success", response)
-      } else {
-        result.cause().printStackTrace()
-      }
-    })
-})
-
-//Consumer para mandar email desde servicio
-eb.consumer("com.makingdevs.emailer.send.service", { message ->
-  //Engine, match with params
-  //Inicializar el engine
-  def engine=new groovy.text.SimpleTemplateEngine()
-  //hacer el match, e imprimir el resultado
-  def contentEmail=engine.createTemplate(message.body().content).make(message.body().params)
-  message.reply(contentEmail.toString())
-
-  //Armar el email
-  def mail=[:]
-
-  def response="Hemos enviado lo siguiente:\n TEMPLATE ID:"+message.body().id+".\n DESTINATARIO: "+message.body().to+"\n SUBJECT: "+message.body().subject
-
-  if(message.body().cc) {
-    mail.cc=message.body().cc
-    response=response+" \n CC: "+mail.cc
-  }
-
-  if(message.body().cco){
-    mail.bcc=message.body().cco
-    response=response+" \n CCO: "+mail.bcc
-  }
-
-  mail.from="Emailer@app.com"
-  mail.to=message.body().to
-  mail.subject=message.body().subject
-  mail.html=contentEmail.toString()
-
-  mailClient.sendMail(mail, { result ->
-    if (result.succeeded()) {
-      vertx.eventBus().send("com.makingdevs.email.success", response)
-    } else {
-      result.cause().printStackTrace()
-    }
-  })
-})
-
 //Consumer para encargarse de armar y mandar el correo
 //Recibe: [:] con valores del email encontrado
 //Salida: Llama a los otros dos verticles para realizar el servicio
