237ede95c08d082d0c04d53d8ffc7ec66eed71d4
neodevelop <juan@makingdevs.com>
2016-11-17 10:42:56 -0600

    #27 agregando el MainVerticle

---
+++ src/main/groovy/com/makingdevs/MainVerticle.groovy
@@ -0,0 +1,17 @@
+package com.makingdevs
+
+import io.vertx.core.AbstractVerticle
+import io.vertx.core.DeploymentOptions
+
+class MainVerticle extends AbstractVerticle {
+  @Override
+  void start(){
+    def config = vertx.currentContext().config()
+    DeploymentOptions options = new DeploymentOptions().setConfig(config)
+    if(!config.map['mail'] || !config.map['mongo'])
+      throw new RuntimeException("""\
+      Cannot run withouit config, check https://github.com/makingdevs/emailer-app/wiki/Emailer-App
+     """)
+    vertx.deployVerticle("webserver.groovy", options)
+  }
+}
