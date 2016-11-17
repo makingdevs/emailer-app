7230f2790a63bc1fae68a0240dcf2088272c3987
neodevelop <juan@makingdevs.com>
2016-11-17 11:32:04 -0600

    #27 adding a simple test(not working)

---
+++ src/test/groovy/com/makingdevs/VerticlesTest.groovy
@@ -0,0 +1,35 @@
+package com.makingdevs
+
+import io.vertx.core.Vertx
+import io.vertx.ext.unit.Async
+import io.vertx.ext.unit.TestContext
+import io.vertx.ext.unit.junit.VertxUnitRunner
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(VertxUnitRunner.class)
+class MainVerticleTest {
+
+  Vertx vertx
+
+  @Before
+  void setUp(TestContext tc) {
+    vertx = Vertx.vertx()
+    vertx.deployVerticle(HelperVerticle.class.getName(), tc.asyncAssertSuccess())
+  }
+
+  @After
+  void tearDown(TestContext tc) {
+    vertx.close(tc.asyncAssertSuccess())
+  }
+
+  @Test
+  void testThatTheServerIsStarted(TestContext tc) {
+    Async async = tc.async()
+    tc.assertTrue(4 > 0)
+    async.complete()
+  }
+
+}
