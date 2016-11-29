85851c43cf86b1023fb51732695e31c488ac9979
carlogilmar <carlogilmar12@gmail.com>
2016-11-29 13:09:14 -0600

    #28 Agregando debug en verticle js

--- src/main/resources/webroot/js/verticle.js
+++ src/main/resources/webroot/js/verticle.js
@@ -1,7 +1,8 @@
 var eb = new EventBus(window.APP.url + '/eventbus');
+console.log("Verticle Javascript Vertx");
 eb.onopen = function() {
   eb.registerHandler('com.makingdevs.email.success', function(error, message) {
-
+   console.log("Verticle funcionando");
     new PNotify({
       title: 'Se ha enviado el correo exitosamente.',
       text: 'Mensaje: '+message.body,
