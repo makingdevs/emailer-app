4f3440108e53a090f1fb7986697b23ce01eab4bb
carlogilmar <carlogilmar12@gmail.com>
2016-11-29 13:43:44 -0600

    #28 Clean code

--- src/main/resources/webroot/js/verticle.js
+++ src/main/resources/webroot/js/verticle.js
@@ -2,9 +2,8 @@ var eb = new EventBus(window.APP.url + '/eventbus');
 eb.onopen = function() {
   eb.registerHandler('com.makingdevs.email.success', function(error, message) {
     new PNotify({
-      title: 'Se ha enviado el correo exitosamente.',
+      title: 'Mensaje de Emailer App v2',
       text: 'Mensaje: '+message.body,
-      type: 'success'
     });
   });
 }
