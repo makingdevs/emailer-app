f736ef8c66068d1ca988ab9c84b7936e3772c77f
carlogilmar <carlogilmar12@gmail.com>
2017-01-20 15:16:39 -0600

    #31 Agregand script js de prueba

---
+++ src/main/resources/webroot/js/prueba.js
@@ -0,0 +1,34 @@
+/*
+console.log ("Pruebas aqui");
+var readEmails=function(){
+	alert("Leyendo Emails");
+	//show and hide divs
+}
+
+// route for new Email
+var prueba1=function(){
+	alert("Prueba 1");
+}
+var prueba2=function(){
+	alert("Prueba 2");
+}
+var prueba3=function(){
+	alert("Prueba 3");
+}
+
+//Routes Director JS
+var routes = {
+	'/': readEmails,
+	'/prueba1': prueba1,
+	'/prueba2': prueba2,
+	'/prueba3': prueba3
+};
+
+var router = Router(routes);
+router.init();
+*/
+console.log("tiny");
+tinymce.init({
+      selector: "textarea",
+          themes: "modern"
+});
